5d4c38ef6833f4848b3233988143237600c81e9c
DESKTOP-NU011FB\DELL <phamanh98@outlook.com>
2023-05-03 14:20:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { loadPosts } from './redux/actions/postAction';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //dispatch action
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadPosts());
+  }, [dispatch]);
+
+  //selector state
+  const data = useSelector((state) => state.posts.data);
+  const requesting = useSelector((state) => state.posts.requesting);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <header className="App-header">
+        {
+          requesting ?
+            <img src={logo} className="App-logo" alt="logo" />
+            :
+            (data && data.length > 0) ? <div>
+              <ul className="list-group">
+                {data.map(item =>
+                  <li key={item.id} className="list-group-item">{item.title}</li>
+                )}
+              </ul>
+            </div>
+              : <div>Data is empty</div>
+        }
       </header>
     </div>
   );
---
+++ src/redux/constants/post.js
@@ -0,0 +1,4 @@
+// post.js
+export const FETCH_POST_REQUEST = "FETCH_POST_REQUEST";
+export const FETCH_POST_SUCCESS = "FETCH_POST_SUCCESS";
+export const FETCH_POST_ERROR = "FETCH_POST_ERROR";
